8e0bd9690a50470fd8680653f2d30d80c05fd776
zpeng <zpeng0614@gmail.com>
2024-12-24 19:34:09 +0800

    web函数完善，添加注释

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 #include <ESPNOW.hpp>
 #include <HXCthread.hpp>
-
+#include <WebService.hpp>
 
 
 void setup() {
   esp_now_setup();//初始化espnow通信
   Serial.begin(115200);//初始化串口通信
-
-
+  esp32_ap_setup();
+  esp32_web_create();
 }
 
 void loop() {
-
+       
 
 
 }
